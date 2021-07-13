4eefac71a55f7aa261ab4cf4a5efdc6a8900a890
pcapriles <pedro.capriles@walmart.com>
2021-07-13 08:48:55 -0400

    ES6 Import Statements and configuration gitlab-ci

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+import React from 'react';
 // Create a new Component. This component should produce
 // some HTML
 const App = function() {
     return <div>Hi!</div>
 }
+
 // Take this component's generated HTML and got it
 // on the page (in the DOM)
+
+React.render(App);
