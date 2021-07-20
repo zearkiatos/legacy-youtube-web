74e79173f7a2525c8c84dfa14b14a3355729d1ec
pcapriles <pedro.capriles@walmart.com>
2021-07-20 08:29:21 -0400

    More on State

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,14 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class SearchBar extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = { term: '' };
-    }
-    render() {
-        return <input onChange={(event) => console.log(event.target.value)} />;
-    }
+  constructor(props) {
+    super(props);
+    this.state = { term: "" };
+  }
+  render() {
+    return (
+      <div>
+        <input
+          onChange={(event) => this.setState({ term: event.target.value })}
+        />ƒ
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
