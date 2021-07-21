3d5d5d73cc2c0107a3bc5bae85c173bf4ec0924a
pcapriles <pedro.capriles@walmart.com>
2021-07-21 08:43:35 -0400

    Controlled Components

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -3,14 +3,15 @@ import React, { Component } from "react";
 class SearchBar extends Component {
   constructor(props) {
     super(props);
-    this.state = { term: "" };
+    this.state = { term: "Starting Value" };
   }
   render() {
     return (
       <div>
         <input
+          value={this.state.term}
           onChange={(event) => this.setState({ term: event.target.value })}
-        />ƒ
+        />
       </div>
     );
   }
