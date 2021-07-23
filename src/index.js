a0ee70d16e51d861f82ff525636317b31ce60ac9
pcapriles <pedro.capriles@walmart.com>
2021-07-23 08:33:17 -0400

    Youtube Search Response

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/SearchBar';
 import config from './config';
+import YoutubeSearch from 'youtube-api-search';
 
+YoutubeSearch({key: config.YOUTUBE_API_KEY,  term: 'surfboards'}, function(data) {
+    console.log(data);
+});
 const App = () => {
     return (
         <div>
