30a508598a65a30d3d24dd590108e6c490a8aacf
zearkiatos <caprilespe@outlook.com>
2021-07-24 08:04:39 -0400

    Refactoring Fucntional Components to Class Components and add kubernetes configuration

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/SearchBar';
 import config from './config';
 import YoutubeSearch from 'youtube-api-search';
 
-YoutubeSearch({key: config.YOUTUBE_API_KEY,  term: 'surfboards'}, function(data) {
-    console.log(data);
-});
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videos: []
+        };
+
+        YoutubeSearch({ key: config.YOUTUBE_API_KEY, term: 'surfboards' }, (videos) => {
+            this.setState({ videos })
+        });
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
