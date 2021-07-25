8c1f33150940bb56585b2265d9ad843879965f98
zearkiatos <caprilespe@outlook.com>
2021-07-25 15:56:47 -0400

    Detail component and Template Strings

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,7 @@
 const config = {
     ENVIRONMENT: process.env.NODE_ENV,
-    YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY
+    YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY,
+    YOUTUBE_BASE_URL: process.env.YOUTUBE_BASE_URL
 };
 
 export default config;
