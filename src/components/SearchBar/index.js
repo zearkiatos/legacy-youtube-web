a30ce1d16124992710e64c971846a564eaa4ee63
zearkiatos <caprilespe@outlook.com>
2021-07-28 19:41:01 -0400

    Styling with CSS and Add docker build step into github action

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -3,11 +3,11 @@ import React, { Component } from "react";
 class SearchBar extends Component {
   constructor(props) {
     super(props);
-    this.state = { term: "Starting Value" };
+    this.state = { term: "" };
   }
   render() {
     return (
-      <div>
+      <div className="search-bar">
         <input
           value={this.state.term}
           onChange={(event) => this.setState({ term: event.target.value })}
--- src/components/VideoDetail/index.js
+++ src/components/VideoDetail/index.js
@@ -6,7 +6,7 @@ const VideoDetail = ({ video }) => {
         const videoId = video.id.videoId;
         const url = config.YOUTUBE_BASE_URL + videoId;
         return (
-            <div className="video-detail col-md-8">
+            <div className="video-detail col-md-12">
                 <div className="embed-responsive embed-responsive-16by9">
                     <iframe className="embed-responsive-item" src={url}></iframe>
                 </div>
