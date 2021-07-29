ddfc2566e300c3752c5258b4fb019ca38881929e
Pedro Capriles M <p0c04cw@M-C02Z9029LVCG.local>
2021-07-29 08:48:51 -0400

    Searching for Videos

--- src/index.js
+++ src/index.js
@@ -1,39 +1,43 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import SearchBar from './components/SearchBar';
-import config from './config';
-import VideoList from './components/VideoList';
-import YoutubeSearch from 'youtube-api-search';
-import VideoDetail from './components/VideoDetail';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import SearchBar from "./components/SearchBar";
+import config from "./config";
+import VideoList from "./components/VideoList";
+import YoutubeSearch from "youtube-api-search";
+import VideoDetail from "./components/VideoDetail";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            selectedVideo: null,
-            videos: []
-        };
+    this.state = {
+      selectedVideo: null,
+      videos: [],
+    };
 
-        YoutubeSearch({ key: config.YOUTUBE_API_KEY, term: 'surfboards' }, (videos) => {
-            this.setState({
-                videos,
-                selectedVideo: videos[0]
-            });
-        });
-    }
-    render() {
-        return (
-            <div>
-                <SearchBar />
-                <VideoDetail video={this.state.selectedVideo} />
-                <VideoList
-                    onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
-                    videos={this.state.videos}
-                />
-            </div>
-        );
-    }
+    this.videoSearch("SOLID Principles");
+  }
+
+  videoSearch(term) {
+    YoutubeSearch({ key: config.YOUTUBE_API_KEY, term }, (videos) => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0],
+      });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar onSearchTermChange={(term) => this.videoSearch(term)} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={(selectedVideo) => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector(".container"));
